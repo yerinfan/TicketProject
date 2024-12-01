a40a72026d8c7f3ab107cee40ba6107e99d0252a
Lee YeEun <78542145+yerinfan@users.noreply.github.com>
2024-12-02 08:13:01 +0900

    디렉토리이동

--- java/kr/ac/kopo/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface Controller {
-
-	String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
